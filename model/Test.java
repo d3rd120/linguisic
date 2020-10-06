4b5b9e05e53a607aff2bcc93839c1c2acec09ca2
KrimssmirK <72426749+KrimssmirK@users.noreply.github.com>
2020-10-06 12:58:23 +0900

    Test class
    i created a class for test

---
+++ model/Test.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Test{
+    private String name;
+
+    public Test(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
